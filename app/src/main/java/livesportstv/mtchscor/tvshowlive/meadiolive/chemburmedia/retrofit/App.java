f7b099ee685381b8f8e53be48a21482c85bc6fd3
lachung305 <mittalgelaani43@gmail.com>
2023-12-13 13:34:13 +0530

    ads multiple id created with code updated

---
+++ app/src/main/java/livesportstv/mtchscor/tvshowlive/meadiolive/chemburmedia/retrofit/App.java
@@ -0,0 +1,20 @@
+
+package livesportstv.mtchscor.tvshowlive.meadiolive.chemburmedia.retrofit;
+
+
+
+import leaflist.showcard.meadiablink.chemburplacard.ChemburApp;
+
+public class App extends ChemburApp {
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+}
